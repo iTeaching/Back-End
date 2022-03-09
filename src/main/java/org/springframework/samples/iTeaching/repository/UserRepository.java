9359784c31a4fc201935cdcc90db24ae7ff1e4dd
FranAnillo <60961813+FranAnillo@users.noreply.github.com>
2022-03-09 12:48:26 +0100

    Actualización de usuarios
    Modificación para que se creen usuarios con los roles específicos de alumno y profesor

--- src/main/java/org/springframework/samples/iTeaching/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.samples.iTeaching.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.samples.iTeaching.model.User;
-
-
-public interface UserRepository extends  CrudRepository<User, String>{
-	
-}
