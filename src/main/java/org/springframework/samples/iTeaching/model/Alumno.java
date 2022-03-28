9551ace5f72717991eb937a711c4eebf75d08b56
FranAnillo <60961813+FranAnillo@users.noreply.github.com>
2022-03-28 11:10:58 +0200

    Cambio para la asignatura

--- src/main/java/org/springframework/samples/iTeaching/model/Alumno.java
+++ src/main/java/org/springframework/samples/iTeaching/model/Alumno.java
@@ -1,21 +1,4 @@
-/*
- * Copyright 2002-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.springframework.samples.iTeaching.model;
-
-
 
 import java.util.List;
 import java.util.Set;
@@ -24,7 +7,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -51,7 +33,7 @@ public class Alumno extends Person {
 	private Set<Valoracion> valoraciones;
 		
 	@ManyToMany(mappedBy = "alumnos")
-	List<Sala> salas;
+	List<Asignatura> salas;
 
 	@Override
 	public String toString() {
