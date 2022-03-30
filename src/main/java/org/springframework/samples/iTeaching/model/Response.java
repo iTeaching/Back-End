e7f035eda9c977218bd6b808092649b29f941c8c
Daniel Dorante <dandorsan@alum.us.es>
2022-03-30 22:10:13 +0200

    062-Sistema de descarga de archivos: Create

---
+++ src/main/java/org/springframework/samples/iTeaching/model/Response.java
@@ -0,0 +1,22 @@
+package org.springframework.samples.iTeaching.model;
+
+public class Response {
+
+    private String message;
+
+    public Response(String message) {
+        super();
+        this.message=message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
+    
+}
