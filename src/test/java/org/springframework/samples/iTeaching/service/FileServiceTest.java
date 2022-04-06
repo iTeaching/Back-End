f5a37496c91452267999ea9bd1685d603d09ac4f
Blancamauri <Blamaurob@alum.us.es>
2022-04-06 17:43:48 +0200

    FileTestService

--- src/test/java/org/springframework/samples/iTeaching/service/FileServiceTest.java
+++ src/test/java/org/springframework/samples/iTeaching/service/FileServiceTest.java
@@ -1,10 +1,15 @@
 package org.springframework.samples.iTeaching.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 
@@ -13,7 +18,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.Resource;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
+import org.springframework.web.accept.PathExtensionContentNegotiationStrategy;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
@@ -27,18 +35,23 @@ public class FileServiceTest {
 	public void testLoadAll() throws Exception {
 		Stream<Path> p = this.fileService.loadAll();
 
-		assertThat(p.count()==2);
+		assertEquals(2, p.count());
 	}
 //	@Test
 //	public void testSave() throws Exception {
 //		
+//		MultipartFile multipartFile = new MockMultipartFile("file3.pdf", new FileInputStream(new File("")));
+//
+//        this.fileService.save(multipartFile);
+//
+//        assertEquals(3, fileService.loadAll().count());
 //	}
+//	
 	@Test
-	public void testLoad() throws Exception {
-		Resource p = this.fileService.load("file1");
-
-		assertThat(p.equals("/Back-End/uploads/file1.pdf"));
-	}
+    public void testLoad() throws Exception {
+        Resource p = this.fileService.load("file1.pdf");
+        assertEquals(4833471L, p.contentLength());
+    }
 
 
 }
